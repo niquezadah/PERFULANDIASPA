1e10a22241f15830b92bb8ce5a449ac6e02873a4
JaAncaten <ja.ancaten@duocuc.cl>
2026-07-06 19:27:59 -0400

    Agregre la carpeta logistica-service

---
+++ logistica-service/src/test/java/com/perfulandia/logistica/LogisticaServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.perfulandia.logistica;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LogisticaServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
